8fe83f0fe6ef0c0779b42644ce167d98b971c04b
Mohsin khan <mk4128001@gmai.com>
2026-03-24 02:29:48 +0530

    twelth commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -38,7 +38,7 @@ export async function register(req, res) {
     html: `<p>Hi ${username}, </p> Thank you for registering at <strong>Perplexity</strong>.
      We're excited to have you on board\n\n
      <p>Please verify your email address by clicking the link below.</p>
-     <a href="https://perplexity-backend-1-rcvm.onrender.com/api/auth/verify-email?token=${token}" </a>
+     <a href="https://perplexity-backend-1-rcvm.onrender.com/api/auth/verify-email?token=${token}"> </a>
      <p>if you did not create an account , please ignore this email
      <p> Best regards, \n <br> The perplexity Team.</p>  `,
   });
