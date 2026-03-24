f6c8df96ca1d62260021ad17df5d8a0aa30bf4a7
Mohsin khan <mk4128001@gmai.com>
2026-03-24 16:55:55 +0530

    tweentythree

--- src/service/mail.service.js
+++ src/service/mail.service.js
@@ -1,34 +1,65 @@
 // import nodemailer from 'nodemailer'
-import brevo from '@getbrevo/brevo';
+// import * as Brevo from '@getbrevo/brevo';
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
 
 
-const client = new Brevo.TransactionalEmailsApi();
-client.setApiKey(
-    brevo.TransactionalEmailsApiApiKeys.apiKey,
-    process.env.BREVO_API_KEY
-);
+
+
+// const client = new Brevo.TransactionalEmailsApi();
+// client.setApiKey(
+//     Brevo.TransactionalEmailsApiApiKeys.apiKey,
+//     process.env.BREVO_API_KEY
+// );
+
 
 
 export async function sendEmail({ to, subject, html }) {
-    try {
-        const sendSmtpEmail = new Brevo.SendSmtpEmail();
-        sendSmtpEmail.subject = subject;
-        sendSmtpEmail.htmlContent = html;
-        sendSmtpEmail.sender = {
-            name: "Perplexity",
-            email: process.env.BREVO_SENDER_EMAIL
-        };
-        sendSmtpEmail.to = [{ email: to }];
-        const result = await client.sendTransacEmail(sendSmtpEmail);
-        console.log("✅ Email sent:", result.messageId);
-    } catch (err) {
-        console.error("❌ Email failed:", err.message);
-    }
+  try {
+    const res = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Perplexity",
+          email: process.env.BREVO_SENDER_EMAIL,
+        },
+        to: [{ email: to }],
+        subject: subject,
+        htmlContent: html,
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log("✅ Email sent:", res.data);
+  } catch (err) {
+    console.error("❌ Email failed:", err.response?.data || err.message);
+  }
 }
+
+// export async function sendEmail({ to, subject, html }) {
+//     try {
+//         const sendSmtpEmail = new Brevo.SendSmtpEmail();
+//         sendSmtpEmail.subject = subject;
+//         sendSmtpEmail.htmlContent = html;
+//         sendSmtpEmail.sender = {
+//             name: "Perplexity",
+//             email: process.env.BREVO_SENDER_EMAIL
+//         };
+//         sendSmtpEmail.to = [{ email: to }];
+//         const result = await client.sendTransacEmail(sendSmtpEmail);
+//         console.log("✅ Email sent:", result.messageId);
+//     } catch (err) {
+//         console.error("❌ Email failed:", err.message);
+//     }
+// }
 
 // transporter is web server ko SMTP server s connect build kar ta hai communicate karta hai for SMTP server email 
 // bhj sake
